c99d9830a45a624a45b631b47ac68602717eba48
Ramssesk <ramsses_vm@hotmail.com>
2021-11-22 10:43:30 -0600

    add to github

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCn03JTEOXP1s3aBxWp_VkuFMF9goAvid8",
+    authDomain: "producthunt-71eca.firebaseapp.com",
+    databaseURL: "https://producthunt-71eca-default-rtdb.firebaseio.com",
+    projectId: "producthunt-71eca",
+    storageBucket: "producthunt-71eca.appspot.com",
+    messagingSenderId: "159987750320",
+    appId: "1:159987750320:web:0b9b08e3e0419964233275"
+};
+
+export default firebaseConfig;
